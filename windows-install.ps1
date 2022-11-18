8486494cd6f5fb1ff53aa5413c48ea2daa0d2d55
Chirag Pareek <chirag.pareek@justice.gov.uk>
2022-11-18 13:50:24 +0000

    updated

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,12 +1,7 @@
 #install latest version of powershell
 Invoke-Expression "& { $(Invoke-RestMethod 'https://aka.ms/install-powershell.ps1') } -useMSI -EnablePSRemoting -Quiet" 
 
-invoke-expression 'cmd /c start pwsh.exe -Command {
-#Uninstall modules if exist to avoid conflict with older version of Powershell
-Uninstall-Module -Name PSDesiredStateConfiguration -force
-Uninstall-Module -Name AuditPolicyDsc -force
-Uninstall-Module -Name SecurityPolicyDsc -force
-Uninstall-Module -Name NetworkingDsc -force
+
 
 
 
@@ -15,4 +10,4 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -force
 Install-Module -Name AuditPolicyDsc -force
 Install-Module -Name SecurityPolicyDsc -force
 Install-Module -Name NetworkingDsc -force
-Install-Module -Name PSDesiredStateConfiguration -force }'
+Install-Module -Name PSDesiredStateConfiguration -force 
